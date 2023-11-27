4b3a579a50ce558ab9dca57fd3d179558aea95ed
PebbleBuilds <kiribuilds@gmail.com>
2023-11-27 15:43:57 -0800

    moved out helper fns

--- collision_classification/classify_dtw.py
+++ collision_classification/classify_dtw.py
@@ -15,32 +15,8 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 import time
 
-# Load all csv file data from selected directory
-def loadData(path):
-    #path = './csv_data'
-    # Note current working directory for later
-    cwd = os.getcwd()
-    # Find all csv files in specified path
-    extension = 'csv'
-    os.chdir(path)
-    result = glob.glob('*.{}'.format(extension))
-    data = []
-    for f in result:
-        rows = []
-        with open(f, newline='') as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            # Eliminate category names
-            csvreader = [x for x in csvreader][:]
-            for row in csvreader[1:]:
-                # Remove annoying formatting
-                row = str(row)[1:-1].replace("'", '').split(",")
-                # Convert non-empty fields to float
-                row = [float(x) for x in row if x != '']
-                rows.append(row)
-        data.append(rows)
-    # Go back to original working directory before leaving
-    os.chdir(cwd)
-    return data
+from helpers import *
+
 
 # https://stackoverflow.com/questions/57015499/how-to-use-dynamic-time-warping-with-knn-in-python
 def multi_feature_DTW(a, b, num_features=10):
@@ -62,13 +38,6 @@ def multi_feature_DTW(a, b, num_features=10):
             cumdist[ai+1, bi+1] = pointwise_distance[ai,bi] + minimum_cost
 
     return cumdist[an, bn]
-
-# Get rid of time as a feature - everything is already a uniformly sampled time series.
-def removeTimeFeature(data):
-    new_data = []
-    for row in data:
-        new_data.append(row[1:])
-    return new_data
 
 def main():
     categories = [loadData("./csv_data/november_11_collisions/hard_moving"),loadData("./csv_data/november_11_collisions/hard_still"),
---
+++ collision_classification/classify_nn.py
@@ -0,0 +1,32 @@
+import csv, os, glob
+import random
+import numpy as np
+
+import time
+
+# Load all csv file data from selected directory
+def loadData(path):
+    #path = './csv_data'
+    # Note current working directory for later
+    cwd = os.getcwd()
+    # Find all csv files in specified path
+    extension = 'csv'
+    os.chdir(path)
+    result = glob.glob('*.{}'.format(extension))
+    data = []
+    for f in result:
+        rows = []
+        with open(f, newline='') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            # Eliminate category names
+            csvreader = [x for x in csvreader][:]
+            for row in csvreader[1:]:
+                # Remove annoying formatting
+                row = str(row)[1:-1].replace("'", '').split(",")
+                # Convert non-empty fields to float
+                row = [float(x) for x in row if x != '']
+                rows.append(row)
+        data.append(rows)
+    # Go back to original working directory before leaving
+    os.chdir(cwd)
+    return data
