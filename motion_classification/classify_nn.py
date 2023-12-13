79d482d992e7c8f6bc61bd195f3d0d88ebb5ec3e
Patrick Devaney <PatrickDevaney@users.noreply.github.com>
2023-12-13 15:41:22 -0800

    Tried DTW, re-worked neural net

--- motion_classification/classify_nn.py
+++ motion_classification/classify_nn.py
@@ -1,4 +1,5 @@
 import csv, os, glob
+import math
 import random
 import numpy as np
 import numpy.random
@@ -9,7 +10,7 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-
+from tensorflow.keras.regularizers import L2
 
 # Load all csv file data from selected directory
 def loadData(path):
@@ -40,7 +41,18 @@ def loadData(path):
 
 # Make it explicit when we are truncating data
 def dataTrunc(data, n):
-    return data[:n]
+    # Truncate to middle instead of beginning
+    dlen = len(data)
+    diff = (dlen - n) // 2
+    return data[diff:n+diff]
+
+# Zero-pad the data
+def dataPad(data,max_len, n_features):
+    rList = []
+    diff = max_len - len(data)
+    for i in range(diff):
+        rList.append([[0.0] * n_features] * i + data + [[0.0] * n_features] * (diff - i))
+    return rList
 
 # Normalize (i.e. start at 0) the time for each dataset to avoid errors
 def normalizeTime(data):
@@ -77,18 +89,37 @@ def main():
     # Split the data into tuples of processed data and labels
     dataPairs = []
     # The length to truncate each sample at (should find a more elegant solution for this eventually)
-    trunc_len = 199
+    #trunc_len = 199
     # Length to truncate fft at (based on trunc_len)
-    fft_len = 100
+    #fft_len = 100
     # Number of features per sample
     n_features = 11
+    #for i, c in enumerate(categories):
+    #    for sample in c:
+    #        x = transformSample(normalizeTime(dataTrunc(sample, trunc_len)))
+    #        # Add FFT features
+    #        x += rfftFeatures(transformSample(sample), n_features, fft_len)
+    #        y = i
+    #        dataPairs.append((x,y))
+    max_len = 0
+    min_len = math.inf
+    for c in categories:
+        for sample in c:
+            max_len = max(max_len, len(sample))
+            min_len = min(min_len, len(sample))
+    fft_len = max_len // 2
     for i, c in enumerate(categories):
         for sample in c:
-            x = transformSample(normalizeTime(dataTrunc(sample, trunc_len)))
-            # Add FFT features
-            x += rfftFeatures(transformSample(sample), n_features, fft_len)
-            y = i
-            dataPairs.append((x,y))
+            #print(len(sample))
+            #print(len(sample[0]))
+            #exit()
+            pads = dataPad(sample, max_len, n_features)
+            for p in pads:
+                x = transformSample(normalizeTime(p))
+                # Add FFT features
+                x += rfftFeatures(transformSample(sample), n_features, min_len // 2)
+                y = i
+                dataPairs.append((x,y))
     # Shuffle the pairs' order
     random.seed(0)
     np.random.seed(0)
@@ -98,10 +129,10 @@ def main():
     tr_y = []
     test_x = []
     test_y = []
-    for x,y in dataPairs[:-2]:
+    for x,y in dataPairs[:-20]:
         tr_x.append(x)
         tr_y.append(y)
-    for x,y in dataPairs[-2:]:
+    for x,y in dataPairs[-20:]:
         test_x.append(x)
         test_y.append(y)
     # Compose model
@@ -111,21 +142,21 @@ def main():
     test_y = np.array(test_y)
     model = Sequential([
         Flatten(input_shape=(len(tr_x[0]),)),
-        #Dense(512, activation='relu',kernel_regularizer='l2'),
+        #Dense(512, activation='relu',kernel_regularizer=L2()),
         #Dropout(0.3),
-        Dense(256, activation='relu',kernel_regularizer='l2'),
-        #Dropout(0.3),
-        Dense(128, activation='relu',kernel_regularizer='l2'),
-        #Dropout(0.3),
-        Dense(64, activation='relu',kernel_regularizer='l2'),
-        #Dropout(0.3),
-        Dense(32, activation='relu',kernel_regularizer='l2'),
+        Dense(256, activation='relu',kernel_regularizer=L2()),
+        Dropout(0.3),
+        Dense(128, activation='relu',kernel_regularizer=L2()),
+        Dropout(0.3),
+        Dense(64, activation='relu',kernel_regularizer=L2()),
+        Dropout(0.3),
+        Dense(32, activation='relu',kernel_regularizer=L2()),
         #Dropout(0.2),
         Dense(len(categories))
     ])
     loss_fn = SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer=Adam(learning_rate=0.0001), loss=loss_fn, metrics=['accuracy'])
-    model.fit(tr_x, tr_y, epochs=5000)
+    model.fit(tr_x, tr_y, epochs=25)
     model.evaluate(test_x,  test_y, verbose=2)
 
 if __name__ == "__main__":
